94edbba81b810583c4428020c9db56ee02cead62
Apple <apple@MacbookPro.local>
2021-09-30 19:51:35 +0100

    remove connection id that was visible

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ if (port == null || port =="") {
     port = 3000
 }
 mongoose.connect(process.env.URISTRING)
-// mongoose.connect('mongodb+srv://root:test1234@cluster0.zpk7j.mongodb.net/DailyPost?retryWrites=true&w=majority')
     .then((result)=>{
         // listen for requests
         app.listen(port)
